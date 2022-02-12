c9f690035c80fafa434e00887f09567a89d98a89
Kenneth Bennett <Kennybll@outlook.com>
2022-02-12 16:24:53 -0500

    fix: change readJsonFile and writeJsonFile's lib

--- packages/nx-electron/src/utils/generate-package-json.ts
+++ packages/nx-electron/src/utils/generate-package-json.ts
@@ -1,6 +1,5 @@
-import { ProjectGraph } from '@nrwl/devkit';
+import { ProjectGraph, writeJsonFile, readJsonFile } from '@nrwl/devkit';
 import { BuildElectronBuilderOptions } from '../executors/build/executor';
-import { writeJsonFile, readJsonFile } from '@nrwl/workspace/src/utilities/fileutils';
 import { INDEX_OUTPUT_FILENAME } from './config';
 
 /**
